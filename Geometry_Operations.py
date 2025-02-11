85fb7dff2dcc8cbfd15fbe76b80222373fb7c6f0
ltrcey1 <ltracey1@byu.edu>
2025-02-10 17:58:24 -0700

    Add files via upload

--- Geometry_Operations.py
+++ Geometry_Operations.py
@@ -12,48 +12,87 @@ import sys
 
 # length of the beam
 def Length(bar):
-    return
+    bar_node = bar.init_node
+    bar_vec= BarNodeToVector(bar_node,bar)
+    bar_length= VectorTwoNorm(bar_vec)
+    return bar_length
 
 # Find two norm (magnitude) of a vector
 def VectorTwoNorm(vector):
-    return
+    norm = 0
+    for i in range(0,len(vector)):
+        norm+= vector[i]**2
+    return np.sqrt(norm)
 
 # Find a shared node between two bars
 def FindSharedNode(bar_1,bar_2):
-    return
+    if (bar_1.init_node==bar_2.init_node):
+        return bar_1.init_node
+    elif (bar_1.end_node==bar_2.end_node):
+        return bar_1.end_node
+    elif (bar_1.end_node==bar_2.init_node):
+        return bar_1.end_node
+    elif (bar_1.init_node==bar_2.end_node):
+        return bar_1.init_node
+    else:
+        sys.exit("The two input bars do not share a node")
+    
 
 # Given a bar and a node on that bar, find the other node
 def FindOtherNode(node,bar):
-    return
+    if(bar.init_node == node):
+        return bar.end_node
+    elif(bar.end_node == node):
+        return bar.init_node
+    else:
+        sys.exit("the input node is not on the bar")
+
 
 # Find a vector from input node (of the input bar) in the direction of the bar
 def BarNodeToVector(origin_node,bar):
-    return
+    other_node=FindOtherNode(origin_node,bar)
+    origin_loc=origin_node.location
+    other_loc=other_node.location
+    vec=[other_loc[0]-origin_loc[0], other_loc[1]-origin_loc[1]]
+    return vec
 
 # Convert to bars that meet at a node into vectors pointing away from that node
 def BarsToVectors(bar_1,bar_2):
-    return
+    shared_node=FindSharedNode(bar_1, bar_2)
+    vec_1=BarNodeToVector(shared_node, bar_1)
+    vec_2=BarNodeToVector(shared_node, bar_2)
+    return [vec_1, vec_2]
 
 # Cross product of two vectors
 def TwoDCrossProduct(vec1,vec2):
-    return
+    cross_product=vec1[0]*vec2[1]-vec2[0]*vec1[1]
+    return cross_product
 
 # Dot product of two vectors
 def DotProduct(vec1,vec2):
-    return
+    dot_product=0
+    for i in range(0,len(vec1)):
+        dot_product = vec1[i]*vec2[i]+dot_product
+    return dot_product
 
 # Cosine of angle from local x vector direction to other vector
 def CosineVectors(local_x_vec,other_vec):
-    return
+    cos_vec = DotProduct(local_x_vec, other_vec)/(VectorTwoNorm(local_x_vec)*VectorTwoNorm(other_vec))
+    return cos_vec
 
 # Sine of angle from local x vector direction to other vector
 def SineVectors(local_x_vec,other_vec):
-    return
+    sin_vec = TwoDCrossProduct(local_x_vec, other_vec)/(VectorTwoNorm(local_x_vec)*VectorTwoNorm(other_vec))
+    return sin_vec
 
 # Cosine of angle from local x bar to the other bar
 def CosineBars(local_x_bar,other_bar):
-    return
+    vector1, vector2 = BarsToVectors(local_x_bar,other_bar)
+    cosine=CosineVectors(vector1,vector2)
+    return cosine
 
 # Sine of angle from local x bar to the other bar
 def SineBars(local_x_bar,other_bar):
-    return
+    vector1, vector2 = BarsToVectors(local_x_bar, other_bar)
+    sine=SineVectors(vector1, vector2)
+    return sine
--- Method_of_Joints.py
+++ Method_of_Joints.py
@@ -12,22 +12,81 @@ import Geometry_Operations as geom
 
 # Determine the unknown bars next to this node
 def UnknownBars(node):
-    return
+    bars_to_node = node.bar
+    my_unknown_bars = []
+    for the_current_bar in bars_to_node:
+        if the_current_bar.is_computed == False:
+            my_unknown_bars.append(the_current_bar)
+    return my_unknown_bars
 
 # Determine if a node if "viable" or not
 def NodeIsViable(node):
-    return
+    my_unknown_bar=UnknownBars(node)
+    if len(my_unknown_bar)>0 & len(my_unknown_bar)<=2:
+        return True
+    else: 
+        return False
     
 # Compute unknown force in bar due to sum of the
 # forces in the x direction
 def SumOfForcesInLocalX(node, local_x_bar):
+    local_x_vector = geom.BarNodeToVector(node, local_x_bar)
+    x_sum=0
+    #sum of external forces
+    x_sum += node.GetNetXForce()*geom.CosineVectors(local_x_vector, [1,0])
+    x_sum += node.GetNetYForce()*geom.CosineVectors(local_x_vector, [0,1])
+    for Bar in node.bars:
+        if Bar.is_computed == True:
+            x_sum +=Bar.axial_load*geom.SineBars(local_x_bar, Bar)
+    #calculate unknown force
+    other_bar=-1*x_sum
+    local_x_bar.axial_load = other_bar
+    local_x_bar.is_computed == True
     return
 
 # Compute unknown force in bar due to sum of the 
 # forces in the y direction
 def SumOfForcesInLocalY(node, unknown_bars):
+    local_x_bar=unknown_bars[0]
+    other_bar=unknown_bars[2]
+    local_x_vector = geom.BarNodeToVector(node, local_x_bar)
+    y_sum=0
+    #sum of external forces
+    y_sum += node.GetNetXForce()*geom.SineVectors(local_x_vector, [1,0])
+    y_sum += node.GetNetYForce()*geom.SineVectors(local_x_vector, [0,1])
+    for Bar in node.bars:
+        if Bar.is_computed == True:
+            y_sum +=Bar.axial_load*geom.SineBars(local_x_bar, Bar)
+    #calculate unknown force
+    other_bar=-1*y_sum/geom.SineBars(local_x_bar, other_bar)
+    unknown_bars.axial_load = other_bar
+    unknown_bars.is_computed == True
+    return
     return
     
 # Perform the method of joints on the structure
 def IterateUsingMethodOfJoints(nodes,bars):
+    count=1
+    contwhile = False
+    for Bar in bars:
+        if Bar.is_computed == False:
+            contwhile = True
+            break 
+    while contwhile == True: 
+        for node in nodes:
+            if NodeIsViable()== True:
+                unknown_bar = UnknownBars(node)
+                if len(unknown_bar)==2:
+                    SumOfForcesInLocalY(unknown_bar)
+                local_x_bar = unknown_bar
+                SumOfForcesInLocalX(local_x_bar)
+        contwhile = False
+        for Bar in bars:
+            if Bar.is_computed == False:
+                contwhile=True 
+                break 
+        if count <= len(nodes):
+            count += 1
+        else: 
+            sys.exit("Infinite Loop")
     return
